28524e04294c11c438db3429996dffcf695bec94
JetsadaWijit <jetsadawijit@outlook.com>
2024-08-06 14:22:07 +0000

    Added conn

--- mcengine-profile-api/src/main/java/com/github/mcengine/api/listeners/MCEngineProfileListenerJoin.java
+++ mcengine-profile-api/src/main/java/com/github/mcengine/api/listeners/MCEngineProfileListenerJoin.java
@@ -1,16 +1,25 @@
 package com.github.mcengine.api.listeners;
 
+import java.sql.Connection;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
+import com.github.mcengine.MCEngineProfile;
+
 public class MCEngineProfileListenerJoin {
+    public static Connection connection = MCEngineProfile.connection;
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         UUID uuid = player.getUniqueId();
-        String query = "SELECT * FROM profiles WHERE uuid = ?";
+        String table = "profiles";
+        int profileData = 0;
+        // get connection from main class
+        getProfile(uuid, table, connection);
     }
 }
