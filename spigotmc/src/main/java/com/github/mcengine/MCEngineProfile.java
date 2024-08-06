dd6a4db2a6494cb66a31607842935794fef5641f
JetsadaWijit <jetsadawijit@outlook.com>
2024-08-06 14:36:44 +0000

    optimize code

--- spigotmc/src/main/java/com/github/mcengine/MCEngineProfile.java
+++ spigotmc/src/main/java/com/github/mcengine/MCEngineProfile.java
@@ -7,32 +7,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MCEngineProfile extends JavaPlugin {
     public static Connection connection;
 
+    public static String getEnvOrConfig(String envVarName, String configKey) {
+        String value = System.getenv(envVarName);
+        return Optional.ofNullable(value).orElseGet(() -> getConfig().getString(configKey));
+    }
+
     @Override
     public void onEnable() {
         String sqlType = getConfig().getString("sqlType");
 
         if (sqlType.equalsIgnoreCase("mysql")) {
             Class clazz = getClass("com.github.mcengine.MCEngineApiMYSQL");
-            String DB_HOST = System.getenv("DB_HOST");
-            if (DB_HOST == null) {
-                DB_HOST = getConfig().getString("DB_HOST");
-            }
-            String DB_USER = System.getenv("DB_USER");
-            if (DB_USER == null) {
-                DB_USER = getConfig().getString("DB_USER");
-            }
-            String DB_PASS = System.getenv("DB_PASS");
-            if (DB_PASS == null) {
-                DB_PASS = getConfig().getString("DB_PASS");
-            }
-            String DB_PORT = System.getenv("DB_PORT");
-            if (DB_PORT == null) {
-                DB_PORT = getConfig().getString("DB_PORT");
-            }
-            String DB_NAME = System.getenv("DB_NAME");
-            if (DB_NAME == null) {
-                DB_NAME = getConfig().getString("DB_NAME");
-            }
+            String DB_HOST = getEnvOrConfig("DB_HOST", "DB_HOST");
+            String DB_USER = getEnvOrConfig("DB_USER", "DB_USER");
+            String DB_PASS = getEnvOrConfig("DB_PASS", "DB_PASS");
+            String DB_PORT = getEnvOrConfig("DB_PORT", "DB_PORT");
+            String DB_NAME = getEnvOrConfig("DB_NAME", "DB_NAME");
             connection = MCEngineApiMYSQL.getConnection(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT);
         } else {
             getLogger().warning("Unsupported SQL type: " + sqlType);
